93136dcf1ed36b221e973c10ae9e167890ae9a18
Jameson Ernst <j.patrick.ernst@gmail.com>
2015-10-03 19:35:33 -0700

    Start on tests

---
+++ tests/tests.rs
@@ -0,0 +1,18 @@
+
+
+extern crate alternate_future;
+
+use std::thread;
+use alternate_future::{promise_future, Promise, Future};
+
+
+#[test]
+fn fulfillment ()
+{
+    //let (p, f) = promise_future();
+
+    //thread::spawn
+
+}
+
+
