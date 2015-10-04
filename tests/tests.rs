e6b3d61650069c547e219f66625f6ba3c435e68b
Jameson Ernst <j.patrick.ernst@gmail.com>
2015-10-04 01:02:34 -0700

    Add tests

--- tests/tests.rs
+++ tests/tests.rs
@@ -3,16 +3,87 @@
 extern crate alternate_future;
 
 use std::thread;
+use std::cell::Cell;
 use alternate_future::{promise_future, Promise, Future};
 
 
 #[test]
-fn fulfillment ()
+fn present ()
 {
-    //let (p, f) = promise_future();
+    let f = Future::present(5);
+    let i = f.await().unwrap();
 
-    //thread::spawn
+    assert_eq!(i, 5);
+}
 
+
+#[test]
+fn fulfil ()
+{
+    let (p, f) = promise_future();
+
+    thread::spawn(move|| {
+        thread::sleep_ms(100);
+        p.fulfill(5);
+    });
+
+    let i : i32 = f.await().unwrap();
+
+    assert_eq!(i, 5);
+}
+
+
+#[test]
+fn broken ()
+{
+    let (p, f) = promise_future::<i32>();
+
+    thread::spawn(move|| {
+        thread::sleep_ms(100);
+        drop(p);
+    });
+
+    assert!(f.await().is_err());
+}
+
+
+#[test]
+fn early ()
+{
+    let (p, f) = promise_future();
+
+    thread::spawn(move|| {
+        p.fulfill(5);
+    });
+
+    thread::sleep_ms(100);
+    let i : i32 = f.await().unwrap();
+
+    assert_eq!(i, 5);
+}
+
+
+thread_local!{
+    static KEY : Cell<i32> = Cell::new(0)
+}
+
+#[test]
+fn chain ()
+{
+    let (p, f) = promise_future();
+
+    thread::spawn(move|| {
+        KEY.with(|k| k.set(1));
+        thread::sleep_ms(100);
+        p.fulfill(5);
+    });
+
+    let i : i32 = f.then(|i| {
+        KEY.with(|k| assert_eq!(k.get(), 0));
+        i * 2
+    }).await().unwrap();
+
+    assert_eq!(i, 10);
 }
 
 
