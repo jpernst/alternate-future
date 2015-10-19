5f92e0f2634b4c3dbef929f3f4e9444fe759cb55
Jameson Ernst <j.patrick.ernst@gmail.com>
2015-10-18 20:57:15 -0700

    finish threadpool

--- src/threadpool.rs
+++ src/threadpool.rs
@@ -1,4 +1,5 @@
 // Copyright 2015 Jameson Ernst
+//
 // Copyright 2014 The Rust Project Developers. See the COPYRIGHT
 // file at the top-level directory of this distribution and at
 // http://rust-lang.org/COPYRIGHT.
@@ -13,13 +14,11 @@
 //! Modified to prevent blocking: If no threads are
 //! currently available to execute the job, a new thread
 //! will be created on demand.
-//! Modified to 
 
 
-use std::sync::mpsc::{channel, Sender, Receiver};
-use std::sync::Arc;
+use std::sync::mpsc::{sync_channel, SyncSender, Receiver, TrySendError};
+use std::sync::{Arc, Mutex};
 use std::thread;
-use spin::Mutex;
 
 
 trait FnBox
@@ -28,7 +27,7 @@ trait FnBox
 }
 impl<F: FnOnce()> FnBox for F
 {
-    fn call_box(self: Box<F>)
+    fn call_box (self: Box<F>)
     {
         (*self)()
     }
@@ -44,20 +43,20 @@ struct Sentinel<'a>
 }
 impl<'a> Sentinel<'a>
 {
-    fn new(jobs: &'a Arc<Mutex<Receiver<Thunk<'static>>>>) -> Sentinel<'a> {
+    fn new (jobs : &'a Arc<Mutex<Receiver<Thunk<'static>>>>) -> Sentinel<'a> {
         Sentinel {
             jobs: jobs,
             active: true
         }
     }
 
-    fn cancel(mut self) {
+    fn cancel (mut self) {
         self.active = false;
     }
 }
 impl<'a> Drop for Sentinel<'a>
 {
-    fn drop(&mut self)
+    fn drop (&mut self)
     {
         if self.active {
             spawn_in_pool(self.jobs.clone())
@@ -69,15 +68,15 @@ impl<'a> Drop for Sentinel<'a>
 #[derive(Clone)]
 pub struct ThreadPool
 {
-    jobs: Sender<Thunk<'static>>
+    jobs: SyncSender<Thunk<'static>>
 }
 impl ThreadPool
 {
-    pub fn new(threads: usize) -> ThreadPool
+    pub fn new (threads : usize) -> ThreadPool
     {
         assert!(threads >= 1);
 
-        let (tx, rx) = channel::<Thunk<'static>>();
+        let (tx, rx) = sync_channel::<Thunk<'static>>(threads);
         let rx = Arc::new(Mutex::new(rx));
 
         for _ in 0..threads {
@@ -87,25 +86,29 @@ impl ThreadPool
         ThreadPool { jobs: tx }
     }
 
-    pub fn execute<F>(&self, job: F)
+    pub fn execute <F> (&self, job: F)
         where F : FnOnce() + Send + 'static
     {
-        self.jobs.send(Box::new(move || job())).unwrap();
+        let job = Box::new(move|| job());
+        match self.jobs.try_send(job) {
+            Err(TrySendError::Full(job)) => { thread::spawn(|| job.call_box()); },
+            Err(..) => panic!(),
+            Ok(..) => (),
+        }
     }
 }
 
-fn spawn_in_pool(jobs: Arc<Mutex<Receiver<Thunk<'static>>>>)
+fn spawn_in_pool (jobs : Arc<Mutex<Receiver<Thunk<'static>>>>)
 {
-    thread::spawn(move || {
+    thread::spawn (move|| {
         let sentinel = Sentinel::new(&jobs);
 
         loop {
-            let message = {
-                let lock = jobs.lock();
+            let job = {
+                let lock = jobs.lock().unwrap();
                 lock.recv()
             };
-
-            match message {
+            match job {
                 Ok(job) => job.call_box(),
                 Err(..) => break,
             }
